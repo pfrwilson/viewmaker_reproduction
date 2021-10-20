545513c2b1394ee6a2ce4352f1781de370b35589
ayeshans <ayeshanaeem604@gmail.com>
2021-10-20 11:56:57 -0400

    fixes & more training

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,8 +6,7 @@ class TrainClassifier(tf.keras.Model):
     def __init__(self, num_classes, encoder):
         super().__init__()
         self.encoder = encoder
-        self.reg = tf.keras.layers.Dense(num_classes, 
-                                        activation='softmax')
+        self.reg = tf.keras.layers.Dense(num_classes)#, activation='softmax')
         self.encoder.trainable = False 
 
     def call(self, x):

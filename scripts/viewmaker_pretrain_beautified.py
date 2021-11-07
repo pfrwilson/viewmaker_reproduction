cbd2004b8b31de2bce430716eeead8596467cbc3
Paul Wilson <pfrwilson@gmail.com>
2021-11-07 14:16:37 -0500

    train and transfer viewmaker scripts

---
+++ scripts/viewmaker_pretrain_beautified.py
@@ -0,0 +1,83 @@
+
+import tensorflow as tf
+import tensorflow_addons as tfa
+import hydra
+from omegaconf import DictConfig
+
+from src.models.resnet_small_version2 import ResNet18
+from src.models.layers import Identity
+from src.datasets import cifar_10
+from src.models.SimCLR import SimCLR_adversarial
+from src.models.viewmaker import Viewmaker
+
+CONFIG_PATH:str = '../configs'
+CONFIG_NAME:str = 'viewmaker_pretrain_config.yaml'
+
+
+def build_model(input_shape, temperature, embedding_dim, load_filepath=None, 
+viewmaker_loss_weight=1.0, distortion_budget=0.05):
+
+    encoder = ResNet18(
+        input_shape = input_shape,
+        classes=10
+    )
+
+    encoder.fc = Identity()
+
+    projection_head = tf.keras.layers.Dense(embedding_dim)
+
+    preprocessing_layer = cifar_10.get_preprocessing_layer()
+
+    viewmaker = Viewmaker(
+        num_channels = input_shape[-1],
+        distortion_budget = distortion_budget
+    )
+
+    model = SimCLR_adversarial(
+        encoder, 
+        preprocessing_layer, 
+        viewmaker, 
+        projection_head,
+        temperature=temperature, 
+        viewmaker_loss_weight = viewmaker_loss_weight
+    )
+
+    model.build(input_shape=input_shape)
+
+    if load_filepath:
+        model.load_weights(load_filepath)
+
+    return model
+
+
+@hydra.main(config_path = CONFIG_PATH, config_name = CONFIG_NAME)
+def train(args: DictConfig) -> None:
+    
+    dataset = cifar_10.get_unsupervised_dataset(args.batch_size)
+
+    model = build_model(
+        (None, 32, 32, 3), 
+        args.temperature,
+        args.embedding_dim,
+        load_filepath = args.load_filepath
+    )
+
+    optimizer = tfa.optimizers.SGDW(
+        learning_rate = args.learning_rate,
+        weight_decay = args.weight_decay, 
+        momentum = args.momentum
+    )
+    
+    model.compile(optimizer=optimizer)
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=args.log_directory)
+
+    model.fit(dataset, epochs=args.epochs, callbacks=[tensorboard_callback])
+
+    model.save_weights(args.save_filepath)
+
+
+if __name__ == '__main__':
+    train()
+
+
---
+++ scripts/viewmaker_transfer_beautified.py
@@ -0,0 +1,100 @@
+
+import hydra
+from omegaconf.dictconfig import DictConfig
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+from src.models.layers import Identity
+from src.models.transfer_learning import TransferModel
+from scripts.viewmaker_pretrain_beautified import build_model as build_pretrained_viewmaker
+from src.datasets.cifar_10 import get_supervised_dataset, get_unsupervised_dataset
+
+CONFIG_PATH:str = '../configs'
+CONFIG_NAME:str = 'viewmaker_transfer_config'
+
+def build_model(input_shape, temperature, embedding_dim, load_filepath,
+ load_viewmaker_filepath, viewmaker_loss_wight, distortion_budget, num_classes):
+    
+    pretrained_model = build_pretrained_viewmaker(
+        input_shape=input_shape,
+        temperature=temperature, 
+        embedding_dim=embedding_dim,
+        load_filepath=load_viewmaker_filepath,
+        viewmaker_loss_weight=viewmaker_loss_wight, 
+        distortion_budget=distortion_budget
+    )
+    
+    encoder = pretrained_model.encoder
+    encoder.pool = Identity()               # encoder output is pre-pool 4x4x512 feature map
+    preprocessing = pretrained_model.preprocessing_layer
+    classifier = tf.keras.Sequential([
+        tf.keras.layers.Flatten, 
+        tf.keras.layers.Dense(num_classes),
+        tf.keras.layers.Softmax()
+    ])
+    viewmaker = pretrained_model.viewmaker
+    model = TransferModel(
+        encoder, 
+        classifier, 
+        preprocessing,
+        viewmaker=viewmaker
+    )
+
+    model.build(input_shape=input_shape)
+
+    if load_filepath:
+        model.load_weights(load_filepath)
+
+    return model
+
+@hydra.main(config_path=CONFIG_PATH, config_name=CONFIG_NAME)
+def main(args: DictConfig) -> None:
+    
+    (x_train, y_train), (x_test, y_test) = get_supervised_dataset()
+
+    model = build_model(
+        input_shape = (None, 32, 32, 3),
+        temperature = args.temperature,
+        embedding_dim = args.embedding_dim, 
+        load_filepath = args.load.pretrained_classifier_weights, 
+        load_simclr_filepath = args.load.pretrained_simclr_weights,
+        num_classes = 10
+    )
+
+    optimizer = tfa.optimizers.SGDW(
+        weight_decay = args.weight_decay,
+        learning_rate = args.learning_rate,
+        momentum = args.momentum
+    )
+
+    model.compile(
+        optimizer, 
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=['accuracy']
+    )
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=args.log_dir, histogram_freq=1)
+
+    if args.train:
+        
+        model.fit(
+            x=x_train, 
+            y=y_train, 
+            batch_size = args.batch_size,
+            epochs=args.epochs,
+            callbacks=[tensorboard_callback],
+            validation_split=0.2
+        )
+
+        model.save_weights(args.save.classifier_weights)
+
+    model.evaluate(
+        x = x_test, 
+        y = y_test, 
+        batch_size = args.eval_batch_size
+    )
+
+
+if __name__ == '__main__':
+    main()
+

2f412cfa18735327d16dff90d3ff97bd01e502a5
Paul Wilson <paulwilson@Pauls-MacBook-Pro.local>
2021-11-07 12:59:30 -0500

    removed redundant builder function

--- src/models/transfer_learning.py
+++ src/models/transfer_learning.py
@@ -1,37 +1,28 @@
 import tensorflow as tf
 
-def get_transfer_classifier(viewmaker, encoder, num_classes, freeze_encoder_weights=True):
-    return TransferModel(
-        encoder, 
-        tf.keras.Sequential([
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(num_classes),
-        ]),
-        viewmaker=viewmaker,
-        freeze_encoder_weights=freeze_encoder_weights
-    )
-
 class TransferModel(tf.keras.Model):
     def __init__(
         self, 
         encoder,
         classifier,
-        viewmaker = None, #optional augmentation layer for training
+        preprocessing,
+        viewmaker = None, 
         freeze_encoder_weights = True,
         **kwargs
     ):
         super().__init__(**kwargs)
+        self.preprocessing = preprocessing
         self.viewmaker = viewmaker
+        self.viewmaker.trainable=False
         self.encoder = encoder
         if freeze_encoder_weights:
             self.encoder.trainable=False
-        self.classifier = classifier
-        self.softmax = tf.keras.layers.Softmax()
+        self.classifier = classifier               
     
     def call(self, x, training=False):
+        x = self.preprocessing(x, training=True) if training else self.preprocessing(x)
         if training and self.viewmaker:
             x = self.viewmaker(x)
         x = self.encoder(x)
-        logits = self.classifier(x)
-        predictions = self.softmax(logits)
+        predictions = self.classifier(x)
         return predictions
